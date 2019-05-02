e9fd35dc38c00f223cf7880ffd2e17e4c9d60782
Adeneuer <47600196+Adeneuer@users.noreply.github.com>
2019-05-02 09:02:16 +0200

    Update LabExercises.py

--- LabExercises.py
+++ LabExercises.py
@@ -1,3 +1,40 @@
+#EX 2 6 11
+
+#1
+def fib(nums = 100):
+    fiblist = []
+    fiblist.append(0)
+    fiblist.append(1)
+    for i in range(2,nums):
+       fiblist.append(fiblist[i-2]+fiblist[i-1])
+    return fiblist
+
+def main1():
+    alist = fib()
+    print(alist)
+
+#2
+def baseConv(li,b1,b2):
+    newList = []
+    sumB10 = 0
+    for exp in range(len(li)-1,-1,-1):
+        if int(li[-1-exp])<b1:
+            sumB10+=int(li[-1-exp])*pow(b1,exp)
+        else:
+            return "Illegal conversion"
+    while sumB10:
+        newList.insert(0,sumB10%b2)
+        sumB10 = int(sumB10/b2)
+    return newList
+
+def main2():
+    fin = open("thoutgs.txt", 'r')
+    r = fin.readlines()
+    numList = []
+    numList = r[0].split()
+    print(baseConv(numList,int(r[1]),int(r[2])))
+#...
+
 #6
 import string
 
@@ -80,6 +117,32 @@ def main6():
         print(r.strip())
         print(engToMorse(r,morseToEngDict,engToMorseDict))
         print(morseToEng(engToMorse(r,morseToEngDict,engToMorseDict),morseToEngDict,engToMorseDict),end='\n\n')
-        
-main6()
 
+#7
+
+#...
+
+#11
+import math
+
+def isPal(num):
+    snum = str(num)
+    return snum==snum[::-1]
+
+def findPal():
+    pals = []
+    for i in range(999,899,-1):
+        for j in range(i,899,-1):
+            if isPal(i*j):
+                pals.append(i*j)
+    return max(pals)
+
+def main11():
+    print(findPal())
+
+#12
+
+#main1()
+#main2()
+#main6()
+#main11()
